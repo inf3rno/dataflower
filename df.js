7e86d5f88e865a18bfc0f7f55f6844cbdd997ff0
inf3rno <i.dont.give.my@email.addr>
2015-01-05 01:31:52 +0100

    Using a different module pattern hereafter.

--- df.js
+++ df.js
@@ -1,60 +1,63 @@
-var extend = function (Ancestor, properties) {
-    var Descendant = function () {
-        if (this.init instanceof Function)
-            this.init.apply(this, arguments);
-    };
-    Descendant.prototype = Object.create(Ancestor.prototype);
-    if (properties)
-        for (var property in properties)
-            Descendant.prototype[property] = properties[property];
-    Descendant.prototype.constructor = Descendant;
-    Descendant.extend = function (properties) {
-        return extend(this, properties);
-    };
-    return Descendant;
-};
+module.exports = function (NativeObject) {
 
-var Obj = extend(Object);
-var Sequence = Obj.extend({
-    initial: undefined,
-    state: undefined,
-    generator: undefined,
-    init: function (config) {
-        this.state = config.initial;
-        this.generator = config.generator;
-    },
-    get: function () {
-        return this.state;
-    },
-    next: function () {
-        var args = [this.state];
-        args.push.apply(args, arguments);
-        this.state = this.generator.apply(this, args);
-        return this.get();
-    },
-    wrap: function () {
-        var store = [];
-        store.push.apply(store, arguments);
-        return function () {
-            var args = [];
-            args.push.apply(args, store);
+    var extend = function (Ancestor, properties) {
+        var Descendant = function () {
+            if (this.init instanceof Function)
+                this.init.apply(this, arguments);
+        };
+        Descendant.prototype = NativeObject.create(Ancestor.prototype);
+        if (properties)
+            for (var property in properties)
+                Descendant.prototype[property] = properties[property];
+        Descendant.prototype.constructor = Descendant;
+        Descendant.extend = function (properties) {
+            return extend(this, properties);
+        };
+        return Descendant;
+    };
+
+    var Object = extend(NativeObject);
+
+    var Sequence = Object.extend({
+        initial: undefined,
+        state: undefined,
+        generator: undefined,
+        init: function (config) {
+            this.state = config.initial;
+            this.generator = config.generator;
+        },
+        get: function () {
+            return this.state;
+        },
+        next: function () {
+            var args = [this.state];
             args.push.apply(args, arguments);
-            return this.next.apply(this, args);
-        }.bind(this);
-    }
-});
+            this.state = this.generator.apply(this, args);
+            return this.get();
+        },
+        wrap: function () {
+            var store = [];
+            store.push.apply(store, arguments);
+            return function () {
+                var args = [];
+                args.push.apply(args, store);
+                args.push.apply(args, arguments);
+                return this.next.apply(this, args);
+            }.bind(this);
+        }
+    });
 
-var uniqueId = new Sequence({
-    generator: function (previousId) {
-        return ++previousId;
-    },
-    initial: 0
-}).wrap();
+    var uniqueId = new Sequence({
+        generator: function (previousId) {
+            return ++previousId;
+        },
+        initial: 0
+    }).wrap();
 
-var df = {
-    Object: Obj,
-    Sequence: Sequence,
-    uniqueId: uniqueId
-};
+    return {
+        Object: Object,
+        Sequence: Sequence,
+        uniqueId: uniqueId
+    };
 
-module.exports = df;
+}(Object);
